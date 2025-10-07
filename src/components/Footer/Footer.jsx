b082b69fd170bf9f4bebe1783d103415fb4ee9ac
Diurno <rafagonpin317@gmail.com>
2025-10-07 11:48:47 -0300

    feat[CDB-69]: Footer iniciado

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,20 @@
 import styles from './Footer.module.css'
 
 const Footer = () => {
-    
+    return(
+        <footer id={styles.footer}>
+            <div className="container-xxl">
+                <div className={styles.containerTecsys}>
+                    <a href="">TecSys Website</a>
+                </div>
+                
+                <div className={styles.containerCDB}>
+                    <span>Desenvolvido por </span>
+                    <a href="https://github.com/codedontblow" target='_blank'>Code Don't Blow</a>
+                </div>
+            </div>
+        </footer>
+    )   
 }
 
 export default Footer
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,8 +1,12 @@
 import styles from './Homepage.module.css'
+import Footer from '../../components/Footer/Footer'
 
 function Homepage () {
     return (
-        <h1>Olá Mundo!</h1>
+        <>
+            <h1>Olá Mundo!</h1>
+            <Footer/>
+        </>
     )
 }
 
