ec0f88b3d0d58c19d3add6f5e7dcaf3673247637
Diurno <rafagonpin317@gmail.com>
2025-10-09 10:13:34 -0300

    feat[CDB-84]: Início Homepage

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -2,7 +2,17 @@ import styles from './Homepage.module.css'
 
 function Homepage () {
     return (
-        <h1>Olá Mundo!</h1>
+        <div className='px-md-5'>
+            <h1 className={styles.title}>Boas-Vindas ao Descriptum!</h1>
+            <div className={styles.textContainer}>
+                <p className={styles.mainText}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia adipisci est vitae placeat nobis corrupti omnis magni aspernatur autem tenetur.
+                </p>
+                <p className={styles.text}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas numquam alias sed culpa perspiciatis natus quis voluptate, repudiandae at distinctio accusantium nisi architecto impedit voluptatibus. Fugit tempora dolore dignissimos nobis rerum modi aspernatur vero maiores expedita rem, nesciunt veritatis velit!
+                </p>
+            </div>
+        </div>
     )
 }
 
