de4f3bfa476a7a6941e37799ee93a03d03bc8b58
Diurno <rafagonpin317@gmail.com>
2025-10-09 10:40:51 -0300

    feat[CDB-84]: Preparando para futura integração com novos componentes

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -2,7 +2,7 @@ import styles from './Homepage.module.css'
 
 function Homepage () {
     return (
-        <div className='px-md-5'>
+        <div className={`px-md-5 ${styles.container}`} >
             <h1 className={styles.title}>Boas-Vindas ao Descriptum!</h1>
             <div className={styles.textContainer}>
                 <p className={styles.mainText}>
@@ -11,6 +11,12 @@ function Homepage () {
                 <p className={styles.text}>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas numquam alias sed culpa perspiciatis natus quis voluptate, repudiandae at distinctio accusantium nisi architecto impedit voluptatibus. Fugit tempora dolore dignissimos nobis rerum modi aspernatur vero maiores expedita rem, nesciunt veritatis velit!
                 </p>
+            </div>
+
+            <div className={styles.buttonsContainer}>
+                {/* O outro botão virá assim (precisa do componente de Botão) */}
+                {/* <a href="" className={styles.userGuideBtn} style={{backgroundColor: 'var(--base-royal)', flexGrow: 1}}>Começar a Extrair</a> */}
+                <a href="" className={styles.userGuideBtn}>Ver Guia de Uso</a>
             </div>
         </div>
     )
