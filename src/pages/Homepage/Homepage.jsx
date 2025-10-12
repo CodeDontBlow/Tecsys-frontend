2b96c78fc1ab3302d25fd0477eefe60843d3d7ac
unknown <rafagonpin317@gmail.com>
2025-10-12 18:49:20 -0300

    feat[CDB-84]: Adicionando botões provsórios e responsividade

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,9 +1,14 @@
 import styles from './Homepage.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronRight , faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 
 function Homepage () {
     return (
         <div className={`container-sm ${styles.container}`}>
-            <h1 className={styles.title}>Boas-Vindas ao <br/> Descriptum</h1>
+            <h1 className={styles.title}>Boas-Vindas ao 
+                <span className={styles.descriptum}>Descriptum</span>
+            </h1>
+            
             <div className={styles.textContainer}>
                 <p className={styles.mainText}>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia adipisci est vitae placeat nobis corrupti omnis magni aspernatur autem tenetur.
@@ -14,9 +19,16 @@ function Homepage () {
             </div>
 
             <div className={styles.buttonsContainer}>
-                {/* O outro botão virá assim (precisa do componente de Botão) */}
-                {/* <a href="" className={styles.userGuideBtn} style={{backgroundColor: 'var(--base-royal)', flexGrow: 1}}>Começar a Extrair</a> */}
-                <a href="" className={styles.userGuideBtn}>Ver Guia de Uso</a>
+                {/* O outro botão virá aqui (precisa do componente de Botão) */}
+                {/* Botão provisório */}
+                <a href="" className={styles.startPorccessBtn}> 
+                    Começar a Extrair 
+                    <FontAwesomeIcon icon={faChevronRight} className={styles.icon}/> 
+                </a>
+                <a href="" className={styles.userGuideBtn}>
+                    Ver Guia de Uso
+                    <FontAwesomeIcon icon={faArrowUpRightFromSquare} className={styles.icon} />
+                </a>
             </div>
         </div>
     )
