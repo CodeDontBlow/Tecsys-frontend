5951a399e7ff92fb344ee7e27cece8b33aa07452
Doryumi <vanessarodriguess23@hotmail.com>
2025-10-24 10:23:57 -0300

    feature[CDB-86]: finalizando componente Card e página DataBase

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,9 @@
 import styles from './Card.module.css'
 
-const Card = ({title, description, icon}) => {
+const Card = ({title, description, icon, horizontal=false}) => {
 
 return (
-    <div className={styles['card']}>
+    <div className={`${styles['card']} ${horizontal ? styles['card-horizontal'] : ''}`}>
         <div className={styles['card-icon']}>{icon}</div>
         <div className={styles['card-content']}>
             <h1>{title}</h1>
--- src/pages/DataBase/DataBase.jsx
+++ src/pages/DataBase/DataBase.jsx
@@ -1,20 +1,39 @@
 import styles from './DataBase.module.css';
 import Card from '../../components/Card/Card';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDolly, faTruck, faIndustry, faClockRotateLeft} from '@fortawesome/free-solid-svg-icons';
 
 function DataBase() {
     return (
 
         <div className={`container-sm ${styles.container}`}>
             <h1>Banco de Dados</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultrices velit dapibus sapien semper, et ornare tellus interdum, et ornare tellus interdum.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultrices velit dapibus sapien semper, et ornare tellus interdum, et ornar e tellus interdum.</p>
             <div className={`${styles.cardArea}`}>
-                <Card icon={''} title={"Produtos"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}></Card>
-                <Card icon={''} title={"Fornecedores"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}></Card>
-                <Card icon={''} title={"Fabricantes"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}></Card>
+                <Card 
+                    icon={<FontAwesomeIcon icon={faClockRotateLeft}/>} 
+                    title="Histórico de Extrações" 
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit." 
+                    horizontal={true}>
+                </Card>
+
+                <Card 
+                    icon={<FontAwesomeIcon icon={faDolly}/>} title="Produtos" 
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit">
+                </Card>
+
+                <Card 
+                    icon={<FontAwesomeIcon icon={faTruck}/>} 
+                    title="Fornecedores" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit.">
+                </Card>
+
+                <Card 
+                    icon={<FontAwesomeIcon icon={faIndustry}/>} 
+                    title="Fabricantes" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit.">
+                </Card>
             </div>
         </div>
-
-)
+    )
 }
 
 export default DataBase;
