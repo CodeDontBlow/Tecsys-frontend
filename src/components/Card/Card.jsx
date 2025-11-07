2e32021ede9191dc7c91faed92a8017deb5f5b78
Doryumi <vanessarodriguess23@hotmail.com>
2025-11-07 10:44:44 -0300

    criando atributo/funcao para card clicavel

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,10 @@
 import styles from './Card.module.css'
 
-const Card = ({title, description, icon, horizontal=false}) => {
+const Card = ({title, description, icon, horizontal=false, onClick}) => {
+
 
 return (
-    <div className={`${styles['card']} ${horizontal ? styles['card-horizontal'] : ''}`}>
+    <div onClick={(onClick)} className={`${styles['card']} ${horizontal ? styles['card-horizontal'] : ''}`}>
         <div className={styles['card-icon']}>{icon}</div>
         <div className={styles['card-content']}>
             <h1>{title}</h1>
--- src/pages/DataBase/DataBase.jsx
+++ src/pages/DataBase/DataBase.jsx
@@ -1,35 +1,41 @@
+import { useNavigate } from 'react-router-dom';
+
 import styles from './DataBase.module.css';
 import Card from '../../components/Card/Card';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDolly, faTruck, faIndustry, faClockRotateLeft} from '@fortawesome/free-solid-svg-icons';
 
 function DataBase() {
-    return (
+    const navigate = useNavigate();
 
+    const handleCardClick = (link) => {
+        navigate(link);
+    };
+
+    return (
         <div className={`container-sm ${styles.container}`}>
             <h1>Banco de Dados</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultrices velit dapibus sapien semper, et ornare tellus interdum, et ornar e tellus interdum.</p>
             <div className={`${styles.cardArea}`}>
                 <Card 
+                    onClick={() => handleCardClick('/')}
                     icon={<FontAwesomeIcon icon={faClockRotateLeft}/>} 
                     title="Histórico de Extrações" 
-                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit." 
+                    description="Clique aqui para exibir o histórico de extrações de dados anteriores." 
                     horizontal={true}>
                 </Card>
-
                 <Card 
                     icon={<FontAwesomeIcon icon={faDolly}/>} title="Produtos" 
-                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit">
+                    description="Clique aqui para exibir a lista de produtos.">
                 </Card>
-
                 <Card 
                     icon={<FontAwesomeIcon icon={faTruck}/>} 
-                    title="Fornecedores" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit.">
+                    title="Fornecedores" description="Clique aqui para exibir a lista de fornecedores.">
                 </Card>
 
                 <Card 
                     icon={<FontAwesomeIcon icon={faIndustry}/>} 
-                    title="Fabricantes" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit.">
+                    title="Fabricantes" description="Clique aqui para exibir a lista de fabricantes.">
                 </Card>
             </div>
         </div>
